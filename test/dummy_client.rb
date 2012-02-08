688bb8cc6fff7fcf49f9aad9b12f5c3ab01ba6b3
John-Paul Stanford <John-Paul.Stanford@arm.com>
2012-02-08 14:34:50 +0000

    More tests

---
+++ test/dummy_client.rb
@@ -0,0 +1,42 @@
+
+require 'net/telnet'
+
+require 'itunesController/controllserver'
+
+class DummyClient
+    
+    DEFAULT_TIMEOUT=40    
+    
+    def connect(hostname,port)
+        @client=Net::Telnet::new('Host' => hostname,
+                                 'Port' => port,
+                                 'Telnetmode' => false)        
+        @client.waitfor('String'=>"001 hello")
+        sendCommand("HELO",220)
+    end
+    
+   
+    def disconnect
+        @client.close
+    end
+    
+    def login(username,password)
+        sendCommand(ItunesController::CommandName::LOGIN+":"+username,222); 
+        sendCommand(ItunesController::CommandName::PASSWORD+":"+password,223); 
+    end
+    
+    def sendCommand(cmd, expectedCode,timeout=DummyClient::DEFAULT_TIMEOUT)
+        @client.cmd(cmd) do | response |            
+            response.each_line do | line |                
+                if ( line =~ /(\d+).*/)                    
+                    code=$1
+                    if (code.to_i==expectedCode)                    
+                        return;
+                    end
+                end
+            end         
+            raise "Did not recive expected response from server"   
+        end        
+    end
+        
+end
--- test/test_server.rb
+++ test/test_server.rb
@@ -3,8 +3,7 @@ require 'dummy_client'
 
 class ServerTest < BaseServerTest
     
-    def test_connect        
-        
+    def test_connect                
         setupServer            
         begin
             assert(!@server.stopped?)
@@ -13,6 +12,7 @@ class ServerTest < BaseServerTest
             client.connect("localhost",@port)                
             assert_equal(1,@server.connections.size)       
             client.disconnect
+            # TODO Fix this so when the client disconnects, we spot it
 #            assert_equal(0,@server.connections.size)
         ensure    
             teardownServer
@@ -20,12 +20,30 @@ class ServerTest < BaseServerTest
         end
     end
     
+    def test_quit                
+            setupServer            
+            begin
+                assert(!@server.stopped?)
+                assert_equal(0,@server.connections.size)    
+                client=DummyClient.new                  
+                client.connect("localhost",@port)                
+                assert_equal(1,@server.connections.size)
+                client.sendCommand(ItunesController::CommandName::QUIT,221)
+                assert_equal(0,@server.connections.size)       
+                client.disconnect                
+            ensure    
+                teardownServer
+                assert(@server.stopped?)
+            end
+        end
+    
     def test_login
         setupServer    
         begin
             client=DummyClient.new
             client.connect("localhost",@port)
-            client.login(BaseServerTest::USER,BaseServerTest::PASSWORD)                   
+            client.login(BaseServerTest::USER,BaseServerTest::PASSWORD)
+            client.sendCommand(ItunesController::CommandName::QUIT,221)                   
             client.disconnect
         ensure    
             teardownServer
