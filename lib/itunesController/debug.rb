e0c0907a859c919b61c2ddd3e7421a119f0ca653
jp.stanford <dev@stanwood.org.uk>
2012-02-23 20:54:09 +0000

    Formatted

--- lib/itunesController/debug.rb
+++ lib/itunesController/debug.rb
@@ -19,63 +19,63 @@
 # License:: GNU General Public License v3 <http://www.gnu.org/licenses/>
 #
 
-
 module ItunesController
+
     class ItunesControllerDebug
-    ANSI_BOLD       = "\033[1m"
-    ANSI_RESET      = "\033[0m"
-    ANSI_LGRAY    = "\033[0;37m"
-    ANSI_GRAY     = "\033[1;30m"
-    
-    # Used to print the methods of a object
-    # @param [Object] obj The object
-    # @param options    
-    def self.pm(obj, *options) 
-          methods = obj.methods
-          methods -= Object.methods unless options.include? :more
-          filter = options.select {|opt| opt.kind_of? Regexp}.first
-          methods = methods.select {|name| name =~ filter} if filter
-    
-          data = methods.sort.collect do |name|
-            method = obj.method(name)
-            if method.arity == 0
-              args = "()"
-            elsif method.arity > 0
-              n = method.arity
-              args = "(#{(1..n).collect {|i| "arg#{i}"}.join(", ")})"
-            elsif method.arity < 0
-              n = -method.arity
-              args = "(#{(1..n).collect {|i| "arg#{i}"}.join(", ")}, ...)"
+        ANSI_BOLD       = "\033[1m"
+        ANSI_RESET      = "\033[0m"
+        ANSI_LGRAY    = "\033[0;37m"
+        ANSI_GRAY     = "\033[1;30m"
+
+        # Used to print the methods of a object
+        # @param [Object] obj The object
+        # @param options
+        def self.pm(obj, *options)
+            methods = obj.methods
+            methods -= Object.methods unless options.include? :more
+            filter = options.select {|opt| opt.kind_of? Regexp}.first
+            methods = methods.select {|name| name =~ filter} if filter
+
+            data = methods.sort.collect do |name|
+                method = obj.method(name)
+                if method.arity == 0
+                    args = "()"
+                elsif method.arity > 0
+                    n = method.arity
+                    args = "(#{(1..n).collect {|i| "arg#{i}"}.join(", ")})"
+                elsif method.arity < 0
+                    n = -method.arity
+                    args = "(#{(1..n).collect {|i| "arg#{i}"}.join(", ")}, ...)"
+                end
+                klass = $1 if method.inspect =~ /Method: (.*?)#/
+                [name, args, klass]
             end
-            klass = $1 if method.inspect =~ /Method: (.*?)#/
-            [name, args, klass]
-          end
-          max_name = data.collect {|item| item[0].size}.max
-          max_args = data.collect {|item| item[1].size}.max
-          data.each do |item|
-            print " #{ANSI_BOLD}#{item[0].to_s.rjust(max_name)}#{ANSI_RESET}"
-            print "#{ANSI_GRAY}#{item[1].ljust(max_args)}#{ANSI_RESET}"
-            print "   #{ANSI_LGRAY}#{item[2]}#{ANSI_RESET}\n"
-          end
-          data.size
-    end
-    
-    # Used to print track information
-    # @param track iTunes track
-    def self.printTrack(track)
-        puts getTrackDescription(track)
-    end
-    
-    # Used to get a track description
-    # @param track iTunes track
-    # @return the track information
-    def self.getTrackDescription(track)
-        pm track
-        if (track.show!=nil && track.show!="")
-            return "TV Show:"+track.show+" - " + track.name
-        else
-            return "Film:"+track.name
+            max_name = data.collect {|item| item[0].size}.max
+            max_args = data.collect {|item| item[1].size}.max
+            data.each do |item|
+                print " #{ANSI_BOLD}#{item[0].to_s.rjust(max_name)}#{ANSI_RESET}"
+                print "#{ANSI_GRAY}#{item[1].ljust(max_args)}#{ANSI_RESET}"
+                print "   #{ANSI_LGRAY}#{item[2]}#{ANSI_RESET}\n"
+            end
+            data.size
         end
-    end
+
+        # Used to print track information
+        # @param track iTunes track
+        def self.printTrack(track)
+            puts getTrackDescription(track)
+        end
+
+        # Used to get a track description
+        # @param track iTunes track
+        # @return the track information
+        def self.getTrackDescription(track)
+            pm track
+            if (track.show!=nil && track.show!="")
+                return "TV Show:"+track.show+" - " + track.name
+            else
+                return "Film:"+track.name
+            end
+        end
     end
 end
