849b22f64deb144e357edb92e0f0475d60c5d558
John-Paul Stanford <John-Paul.Stanford@arm.com>
2012-04-03 15:40:34 +0100

    Make the script more readable

--- jar-stuff.sh
+++ jar-stuff.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+
+GEMS="etc"
+GEMS="$GEMS sqlite3"
+GEMS="$GEMS escape"
+GEMS="$GEMS itunes-controller"
+
+# Compile the gem
 rake
-jruby -S gem install -i ./itunes-controller etc sqlite3 escape itunes-controller --no-rdoc --no-ri
+
+# Create the jar
+jruby -S gem install -i ./itunes-controller $GEMS --no-rdoc --no-ri
 jar cf itunes-controller-dummy-server.jar -C itunes-controller .
 
 rm -rf ./itunes-controller
 
+# Display the results
 jruby -ritunes-controller-dummy-server.jar -S gem list
