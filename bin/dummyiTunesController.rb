1c762f1cb4105dec2609e5ccd2f7d2dbcac3d724
John-Paul Stanford <dev@stanwood.org.uk>
2014-11-23 12:39:08 +0000

    Fixed some errors with the output of the command

--- bin/dummyiTunesController.rb
+++ bin/dummyiTunesController.rb
@@ -56,11 +56,11 @@ class App < ItunesController::Application
 
     # Used to display the command line useage
     def displayUsage()
-        @stdout.puts("Usage: "+@appName+" [options]")
-        @stdout.puts("")
-        @stdout.puts(genericOptionDescription())
-        @stdout.puts("    -p, --port PORT                  The port number to start the server on. Defaults to #{DEFAULT_PORT}")
-        @stdout.puts("    -c, --config FILE                The configuration file")
+        $stdout.puts("Usage: "+@appName+" [options]")
+        $stdout.puts("")
+        $stdout.puts(genericOptionDescription())
+        $stdout.puts("    -p, --port PORT                  The port number to start the server on. Defaults to #{DEFAULT_PORT}")
+        $stdout.puts("    -c, --config FILE                The configuration file")
     end
 
     def checkAppOptions()
